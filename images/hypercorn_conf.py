49709940a8e6c8840f52b96951f5e534d034d5df
adosib <adosib1@gmail.com>
2025-01-22 19:30:53 -0600

    Remove booleanize fn + black formatting

--- images/hypercorn_conf.py
+++ images/hypercorn_conf.py
@@ -1,36 +1,20 @@
 import os
-import multiprocessing
 import json
+import multiprocessing
 
-#utils
-def booleanize(value) -> bool:
-    if value is None:
-        return False
-    
-    falsy = ["no", "n", "0", "false"]
-    truly = ["yes", "y", "1", "true"]
-
-    if value.lower() in falsy:
-        return False
-    elif value.lower() in truly:
-        return True
-    else:
-        raise TypeError("Non boolean-like value {}".format(value))
-
-
-#ssl opts
-use_ssl = booleanize(os.getenv("USE_SSL", "False"))
-use_tcp = booleanize(os.getenv("USE_TCP", "True"))
-#assert any([use_ssl, use_tcp]), "At least one of USE_SSL and USE_TCP must be set"
+# ssl opts
+use_ssl = os.getenv("USE_SSL", False)
+use_tcp = os.getenv("USE_TCP", True)
+# assert any([use_ssl, use_tcp]), "At least one of USE_SSL and USE_TCP must be set"
 
 use_certfile = os.getenv("CERTFILE", None)
 use_ca_certs = os.getenv("CA_CERTS", None)
 use_ciphers = os.getenv("CIPHERS", "ECDHE+AESGCM")
 use_keyfile = os.getenv("KEYFILE", None)
-#assert not(use_ssl and any([use_certfile, use_keyfile, use_ca_certs])), "USE_SSL Requires CERTFILE/KEYFILE/CA_CERTS"
+# assert not(use_ssl and any([use_certfile, use_keyfile, use_ca_certs])), "USE_SSL Requires CERTFILE/KEYFILE/CA_CERTS"
 
 
-#binding
+# binding
 host = os.getenv("HOST", "0.0.0.0")
 ssl_port = os.getenv("SSL_PORT", "443")
 tcp_port = os.getenv("TCP_PORT", "80")
@@ -41,7 +25,7 @@ use_quic_bind = os.getenv("QUIC_BIND", None)
 
 use_insecure_bind = os.getenv("INSECURE_BIND", None)
 
-#assert not(bool(use_insecure_bind) != all([use_ssl, use_tcp])), "INSECURE_BIND Must be used only when USE_SSL and USE_TCP are both set"
+# assert not(bool(use_insecure_bind) != all([use_ssl, use_tcp])), "INSECURE_BIND Must be used only when USE_SSL and USE_TCP are both set"
 if use_ssl and use_tcp:
     if not use_insecure_bind:
         use_insecure_bind = "{}:{}".format(host, tcp_port)
@@ -51,7 +35,7 @@ if not use_bind:
     use_bind = "{}:{}".format(host, ssl_port if use_ssl else tcp_port)
 
 
-#workers
+# workers
 cores = multiprocessing.cpu_count()
 workers_multiplier = float(os.getenv("WORKERS_PER_CORE", "1"))
 workers_max = os.getenv("MAX_WORKERS", None)
@@ -68,20 +52,24 @@ else:
     use_web_concurrency = max(int(default_web_concurrency), 2)
     if workers_max:
         use_web_concurrency = min(use_web_concurrency, workers_max)
-        assert use_web_concurrency > 0, "MAX_WORKERS and WORKERS_PER_CORE Must be non zero"
+        assert use_web_concurrency > 0, (
+            "MAX_WORKERS and WORKERS_PER_CORE Must be non zero"
+        )
 
 use_worker_class = os.getenv("WORKER_CLASS", "asyncio")
-assert use_worker_class in ["asyncio", "uvloop", "trio"], "WORKER_CLASS Must be asyncio, uvloop or trio"
+assert use_worker_class in ["asyncio", "uvloop", "trio"], (
+    "WORKER_CLASS Must be asyncio, uvloop or trio"
+)
 
 
-#others
+# others
 use_graceful_timeout = os.getenv("GRACEFUL_TIMEOUT", "120")
 use_errorlog = os.getenv("ERROR_LOG", "-")
 use_accesslog = os.getenv("ACCESS_LOG", "-")
 use_keepalive_timeout = os.getenv("KEEP_ALIVE", "5")
 
 
-#conf
+# conf
 keep_alive_timeout = int(use_keepalive_timeout)
 worker_class = use_worker_class
 workers = use_web_concurrency
@@ -103,7 +91,7 @@ if use_quic_bind:
     quic_bind = use_quic_bind
 
 
-#conf/env data
+# conf/env data
 conf_data = {
     "accesslog": accesslog,
     "errorlog": errorlog,
@@ -121,10 +109,10 @@ conf_data = {
         "ssl_port": ssl_port if use_ssl else None,
         "tcp_port": tcp_port if use_tcp else None,
         "use_ssl": use_ssl,
-        "use_tcp": use_tcp,        
+        "use_tcp": use_tcp,
         "workers_multiplier": workers_multiplier,
-        "cores": cores
-    }
+        "cores": cores,
+    },
 }
 
-print(json.dumps(conf_data, indent = 4), flush = True)
+print(json.dumps(conf_data, indent=4), flush=True)
